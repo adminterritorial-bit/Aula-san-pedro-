32ceee05a161b800838feedac398fafbcb55f547
adminterritorial-bit <adminterritorial@sanpedro-valle.gov.co>
2026-09-21 19:42:54 -0500

    feat: agregar cliente Supabase publishable

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+(function (w) {
+  if (!w.supabase || !w.supabase.createClient) throw new Error('No fue posible cargar Supabase JS.');
+  var url = 'https://dvdpgllezrmttrknbcjq.supabase.co';
+  var key = 'sb_publishable_u8aF30AdRo_flW3qb-Z8sg_evTHk9Ry';
+  w.AulaSupabase = w.supabase.createClient(url, key, {
+    auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
+  });
+})(window);
